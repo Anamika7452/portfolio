a13ad1090c22b16009607cf7cecf8ad23f8da954
Anamika Meathani <anamikamaithani640@gmail.com>
2022-04-04 16:45:02 +0530

    text-white added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div className="bg-black h-screen ">
-      <h1 className="text-2xl font-medium text-">WELCOME TO MY WORLD</h1>
+      <h1 className="text-2xl text-white font-medium text-">
+        WELCOME TO MY WORLD
+      </h1>
     </div>
   );
 }

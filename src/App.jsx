d8b35309223bbb3c6553aeae73ace978cb5898b4
Anamika Meathani <anamikamaithani640@gmail.com>
2022-04-04 16:40:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-black h-screen ">
+      <h1 className="text-2xl font-medium text-">WELCOME TO MY WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
